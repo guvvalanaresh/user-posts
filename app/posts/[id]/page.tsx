c96c2e866cd419858859f22c11a25e7a95f58b56
guvvalanaresh <gnareshkumarreddy7@gmail.com>
2025-11-08 14:29:59 +0530

    fix bugs related to turbopack and make sure it should work on any os platform

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -16,7 +16,7 @@ export default async function Post({ params }: { params: Promise<{ id: string }>
 
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
-      <article className="max-w-2xl space-y-4 font-[family-name:var(--font-geist-sans)]">
+      <article className="max-w-2xl space-y-4 font-sans">
         <h1 className="text-4xl font-bold mb-8 text-[#333333]">{post.title}</h1>
         <p className="text-gray-600 text-center">by {post.author.name}</p>
         <div className="prose prose-gray mt-8">
