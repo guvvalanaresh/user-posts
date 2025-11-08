474e06669b0cddc2382246ff250f517c55a5a892
guvvalanaresh <gnareshkumarreddy7@gmail.com>
2025-11-08 11:05:48 +0530

    Created posts folder and file

---
+++ app/posts/page.tsx
@@ -0,0 +1,14 @@
+import prisma from "@/lib/prisma";
+
+export default async function Posts() {
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
+      <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] text-[#333333]">
+        Posts
+      </h1>
+      <ul className="font-[family-name:var(--font-geist-sans)] max-w-2xl space-y-4">
+          <li>My first post</li>
+      </ul>
+    </div>
+  );
+}
